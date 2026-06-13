3ff8485dcea9837e5e8714481e284819600dbeff
linsetnaing444 <mgsat437@gmail.com>
2026-06-13 11:15:46 +0630

    update

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # ဒီနေရာမှာ သင့် HTML ဖိုင်ကို ခေါ်လိုက်တာပါ
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
