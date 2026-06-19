5dd382c8fb855476e414f5a873607daf2629c9c8
linsetnaing444 <mgsat437@gmail.com>
2026-06-19 10:40:51 +0630

    update

--- my.js
+++ my.js
@@ -2,6 +2,9 @@ alert("hello မင်္ဂလာပါ")
 alert("ဆက်သွားမလား")
 alert("ဆက်သွားပြီ")
 
-function myFunction() {
-                         alert ("ဆက်သွားရန်ေသချာလား"); 
+function changeColor() {
+                     let element = document.getElementById("demo");
+                     element.style.color = "red";
+                     element.style.backgroundcolor = "yellow";
+                     alert ("အ‌ေရာင်‌ေပြာင်လိုက်ပြီ");
                        }
